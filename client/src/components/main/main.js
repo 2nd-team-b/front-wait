2577686efa072283dfa2d92d1785aaa07f5bbf27
정채림 <chaelimj45@gmail.com>
2023-11-02 16:29:16 +0900

    "feat: git 수정"

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import '../main/main.css';
-import Footer from '../../static/Footer';
+import Footer from '../Footer'
 export default function Main() {
   useEffect(() => {
     AOS.init();
@@ -128,7 +128,7 @@ export default function Main() {
           </div>
         </div>
       </div>
-      <Footer />
+      <Footer/>
     </>
   );
 }
